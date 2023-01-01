d5dc97b9876735dc81c399b09c7287687665c570
M Bilal <bilaldarmuhammad@gmail.com>
2023-01-01 13:13:24 +0500

    complete task

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -1,6 +1,7 @@
 //Section4
 
 import React from "react";
+import axios from "axios";
 function Section4() {
   const [isShown, setIsShown] = React.useState(0);
   const [isVisible, setIsVisible] = React.useState(true);
@@ -17,12 +18,19 @@ function Section4() {
       setIsShown(2);
       //   setIsVisible(false);
     } else if (isShown == 2) {
-      fetchAPI("https://learnwebcode.github.io/json-example/animals-3.json")
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+      // fetchAPI("https://learnwebcode.github.io/json-example/animals-3.json")
+      //   .then((response) => response.json())
+      //   .then((data) => console.log(data));
+      // alert("hello");
+      res();
       setIsShown(0);
       setIsVisible(false);
     }
+  };
+  const res = async () => {
+    await axios
+      .get("https://learnwebcode.github.io/json-example/animals-3.json")
+      .then((data) => console.log(data));
   };
 
   console.log(isShown);
